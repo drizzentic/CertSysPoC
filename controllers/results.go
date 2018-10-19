bfe890181e431fd83bf8181cbf0d51ed7cc0b148
derrick rono <derrickrono@gmail.com>
2018-10-19 16:13:25 +0300

    Added simple results logic to put the created json into a file

--- controllers/results.go
+++ controllers/results.go
@@ -1,25 +1,41 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
-type Course struct {
-	Code         string `json:"code"`
-	Description  string `json:"description"`
-	CF           int    `json:"cf"`
-	Grade        string `json:"grade"`
-	AcademicYear string `json:"academic_year"`
-	Results      `json:"results"`
+type Output struct {
+	Results []struct {
+		Code        string  `json:"code"`
+		Description string  `json:"description"`
+		CF          float64 `json:"cf"`
+		Grade       string  `json:"grade"`
+	} `json:"results"`
+	OverallResults struct {
+		AcademicYear   string  `json:"academic_year"`
+		Averages       float64 `json:"averages"`
+		Cumulative     float64 `json:"cumulative"`
+		Recommendation string  `json:"recommendation"`
+	} `json:"overalls"`
 }
 
-type Results struct {
-	Averages       float64 `json:"averages"`
-	Cumulative     float64 `json:"cumulative"`
-	Recommendation string  `json:"recommendation"`
+type OverallResults struct {
 }
 
 func GetResults(r http.ResponseWriter, w *http.Request) {
 
 }
 func CreateResults(r http.ResponseWriter, w *http.Request) {
+	var s Output
+	body, _ := ioutil.ReadAll(w.Body)
+	//TODO:Create json file for the data and post hash as transaction to blockchain
+	ioutil.WriteFile("output.json", body, 0644)
+	json.Unmarshal(body, &s)
+
+	r.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(r).Encode(s)
 
 }
