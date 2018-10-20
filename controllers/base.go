2736aabaf1ccfe5686712cd90356bd2bfd291f75
derrick rono <derrickrono@gmail.com>
2018-10-20 16:57:52 +0300

    Basic housekeeping and results creation on file and blockchain

--- controllers/base.go
+++ controllers/base.go
@@ -6,6 +6,33 @@ import (
 	"io/ioutil"
 )
 
+type Output struct {
+	Results []struct {
+		Description string  `json:"description"`
+		CF          float64 `json:"cf"`
+		Grade       string  `json:"grade"`
+		Name        string  `json:"fullname"`
+		Code        string  `json:"code"`
+	} `json:"results"`
+	OverallResults struct {
+		Name            string  `json:"fullname"`
+		Code            string  `json:"code"`
+		AcademicYear    string  `json:"academic_year"`
+		Averages        float64 `json:"averages"`
+		Cumulative      float64 `json:"cumulative"`
+		Recommendation  string  `json:"recommendation"`
+		AdmissionNumber string  `json:"admission_number"`
+	} `json:"overalls"`
+}
+type Student struct {
+	Name            string `json:"fullname"`
+	AdmissionNumber string `json:"admission_number"`
+	School          string `json:"school"`
+	Award           string `json:"award"`
+	Department      string `json:"department"`
+	ResultsAddress  string `json:"results_address"`
+}
+
 func processRpcCalls(m string, params []string, data string) Response {
 	var address Response
 
