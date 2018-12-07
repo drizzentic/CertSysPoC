53de5ea80aa3a201b2558644ca86b1a6bca16064
Derrick Rono <derrickrono@gmail.com>
2018-12-07 20:14:28 +0300

    change address to accept all connections

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	//TODO: Initialize API server
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "127.0.0.1:8001",
+		Addr:    "0.0.0.0:8001",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
